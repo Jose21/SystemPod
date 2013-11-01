58cd969c773430de6433039516e536877fd598e2
José Miramar Medina <kokoro.miramar@gmail.com>
2013-11-01 10:02:26 -0600

    se agrega modulo de reportes ejecutivos

---
+++ src/groovy/com/pogos/ConveniosPorFechaBean.groovy
@@ -0,0 +1,14 @@
+package com.pogos
+
+class ConveniosPorFechaBean {
+    
+    //Convenios
+    Integer totalConvenios = 0
+    // Contraidos por el infonavit
+    Integer totalConveniosContraidos = 0
+    // Contraidos no por el infonavit
+    Integer totalConveniosNoContraidos = 0
+    // Titulo de la grafica
+    String title 
+}
+
