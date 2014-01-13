bdeec2ad1cb64de5617840014996b2a16ba939a3
Tomás Moreno <the.real.thom@gmail.com>
2014-01-13 12:59:47 -0600

    bug fix cuando se muestra el campo de tags

--- grails-app/controllers/com/app/sgpod/OtorgamientoDePoderController.groovy
+++ grails-app/controllers/com/app/sgpod/OtorgamientoDePoderController.groovy
@@ -39,22 +39,15 @@ class OtorgamientoDePoderController {
             params.fechaDeOtorgamiento = fechaDeOtorgamiento
         }
         def otorgamientoDePoderInstance = new OtorgamientoDePoder(params)
+        
+        if(otorgamientoDePoderInstance.tags && !otorgamientoDePoderInstance.tags.endsWith(",")){
+            otorgamientoDePoderInstance.tags = otorgamientoDePoderInstance.tags + "," 
+        }
         if (!otorgamientoDePoderInstance.save(flush: true)) {
             render(view: "create", model: [otorgamientoDePoderInstance: otorgamientoDePoderInstance])
             return
         }
         
-        //validacion para la busqueda por tags
-        if(otorgamientoDePoderInstance.tags){
-            if(otorgamientoDePoderInstance.tags.endsWith(",")){
-                params.tags = params.tags.substring(0, params.tags.length() -1)
-            }else {
-                otorgamientoDePoderInstance.tags = otorgamientoDePoderInstance.tags + "," 
-            }
-        }
-        //end busqueda tags
-        
-
         flash.message = message(code: 'default.created.message', args: [message(code: 'poder.label', default: 'Poder'), otorgamientoDePoderInstance.id])
         redirect(action: "edit", id: otorgamientoDePoderInstance.id)
     }
@@ -71,7 +64,9 @@ class OtorgamientoDePoderController {
             redirect(action: "list")
             return
         }
-
+        if(otorgamientoDePoderInstance.tags && otorgamientoDePoderInstance.tags.endsWith(",")){              
+            otorgamientoDePoderInstance.tags = otorgamientoDePoderInstance.tags.substring(0,otorgamientoDePoderInstance.tags.length()-1)
+        }
         [otorgamientoDePoderInstance: otorgamientoDePoderInstance]
     }
 
@@ -82,7 +77,9 @@ class OtorgamientoDePoderController {
             redirect(action: "list")
             return
         }
-        log.info "Edit: anchorpara eliminar->" + params.anchor
+        if(otorgamientoDePoderInstance.tags && otorgamientoDePoderInstance.tags.endsWith(",")){              
+            otorgamientoDePoderInstance.tags = otorgamientoDePoderInstance.tags.substring(0,otorgamientoDePoderInstance.tags.length()-1)
+        }
         [otorgamientoDePoderInstance: otorgamientoDePoderInstance, anchor : params.anchor?:""]
     }
 
@@ -98,7 +95,7 @@ class OtorgamientoDePoderController {
         if (version != null) {
             if (otorgamientoDePoderInstance.version > version) {
                 otorgamientoDePoderInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                          [message(code: 'poder.label', default: 'Poder')] as Object[],
+                    [message(code: 'poder.label', default: 'Poder')] as Object[],
                           "Another user has updated this Poder while you were editing")
                 render(view: "edit", model: [otorgamientoDePoderInstance: otorgamientoDePoderInstance])
                 return
@@ -119,12 +116,8 @@ class OtorgamientoDePoderController {
         otorgamientoDePoderInstance.properties = params
         
         //validacion para la busqueda por tags
-        if(otorgamientoDePoderInstance.tags){
-            if(otorgamientoDePoderInstance.tags.endsWith(",")){
-                params.tags = params.tags.substring(0, params.tags.length() -1)
-            }else {
-                otorgamientoDePoderInstance.tags = otorgamientoDePoderInstance.tags + "," 
-            }
+        if(otorgamientoDePoderInstance.tags && !otorgamientoDePoderInstance.tags.endsWith(",")){
+            otorgamientoDePoderInstance.tags = otorgamientoDePoderInstance.tags + "," 
         }
         //end busqueda tags
 
@@ -132,7 +125,7 @@ class OtorgamientoDePoderController {
             render(view: "edit", model: [otorgamientoDePoderInstance: otorgamientoDePoderInstance])
             return
         }
-         if (params.archivo.getSize()!=0) {            
+        if (params.archivo.getSize()!=0) {            
             def documentoDePoderInstance = new DocumentoDePoder(params)
             documentoDePoderInstance.nombre = params.archivo.getOriginalFilename()
             otorgamientoDePoderInstance.addToDocumentos(documentoDePoderInstance)
@@ -142,6 +135,9 @@ class OtorgamientoDePoderController {
             }
         }
         
+        if(otorgamientoDePoderInstance.tags && otorgamientoDePoderInstance.tags.endsWith(",")){              
+            otorgamientoDePoderInstance.tags = otorgamientoDePoderInstance.tags.substring(0,otorgamientoDePoderInstance.tags.length()-1)
+        }
         flash.message = message(code: 'default.updated.message', args: [message(code: 'poder.label', default: 'Poder'), otorgamientoDePoderInstance.id])
         redirect(action: "edit", id: otorgamientoDePoderInstance.id, params : [ anchor : params.anchor ])
     }
